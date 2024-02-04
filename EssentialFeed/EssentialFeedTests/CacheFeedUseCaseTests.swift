79e3fc58b10bb0e382dd863f0344d0bbc9daf211
haidersyed <haiderrazvi@hotmail.com>
2024-02-05 02:09:56 +0500

    Add memory leak tracking

--- EssentialFeed/EssentialFeedTests/CacheFeedUseCaseTests.swift
+++ EssentialFeed/EssentialFeedTests/CacheFeedUseCaseTests.swift
@@ -5,7 +5,6 @@
 //  Created by Haider Rizvi on 05/02/2024.
 //
 
-
 import Foundation
 import XCTest
 import EssentialFeed
@@ -49,9 +48,11 @@ class CacheFeedUseCaseTests: XCTestCase {
     
     // Mark: - Helper
     
-    private func makeSUT() -> (sut: LocalFeedLoader, store:FeedStore) {
+    private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store:FeedStore) {
         let store = FeedStore()
         let sut = LocalFeedLoader(store: store)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, store)
     }
     
