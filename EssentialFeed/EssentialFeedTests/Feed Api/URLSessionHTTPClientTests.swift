a84ada472325adb9924c709f9415597f0f68a906
haidersyed <haiderrazvi@hotmail.com>
2024-02-08 09:10:40 +0500

    Correct 'UnexpectedValuesRepresentation' access modifier

--- EssentialFeed/EssentialFeedTests/Feed Api/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Api/URLSessionHTTPClientTests.swift
@@ -139,7 +139,7 @@ class  URLSessionHTTPClientTests: XCTestCase {
     }
     
     private func anyData() -> Data  {
-        return Data(bytes: "any data".utf8)
+        return Data(_ : "any data".utf8)
     }
     
     private  func nonHTTPURLResponse() -> URLResponse {
