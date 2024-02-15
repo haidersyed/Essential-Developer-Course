41f9d741cdabe9018a337d0a31050e40258cbe33
haidersyed <haiderrazvi@hotmail.com>
2024-02-15 07:24:44 +0500

    Include cache test helpers in the cache integration tests target

---
+++ EssentialFeed/EssentialFeedTests/Helper/FeedCacheTestHelpers.swift
@@ -0,0 +1,8 @@
+//
+//  FeedCacheTestHelpers.swift
+//  EssentialFeedTests
+//
+//  Created by Haider Rizvi on 15/02/2024.
+//
+
+import Foundation
---
+++ EssentialFeed/EssentialFeedTests/Helper/SharedTestHelpers.swift
@@ -0,0 +1,8 @@
+//
+//  SharedTestHelpers.swift
+//  EssentialFeedTests
+//
+//  Created by Haider Rizvi on 15/02/2024.
+//
+
+import Foundation
