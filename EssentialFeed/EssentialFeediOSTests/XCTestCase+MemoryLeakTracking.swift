81e7cc9a8646501661e3b1026994414e072e03b6
haidersyed <haiderrazvi@hotmail.com>
2024-02-29 08:43:53 +0500

    Feed image view loading indicator is visible while loading image

--- EssentialFeed/EssentialFeediOSTests/XCTestCase+MemoryLeakTracking.swift
+++ EssentialFeed/EssentialFeediOSTests/XCTestCase+MemoryLeakTracking.swift
@@ -6,3 +6,13 @@
 //
 
 import Foundation
+import XCTest
+
+extension XCTestCase {
+
+    func trackForMemoryLeaks(_ instance: AnyObject,file: StaticString = #filePath, line: UInt = #line) {
+        addTeardownBlock {[weak instance] in
+            XCTAssertNil(instance, "Instance should have been deallocated. Potential memory leak.", file: file, line: line)
+        }
+    }
+}
