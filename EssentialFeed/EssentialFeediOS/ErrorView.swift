e7d7d2b224f8ebef91fa9b19595366ef711c20e0
haidersyed <haiderrazvi@hotmail.com>
2024-02-29 19:37:17 +0500

    Move ErrorView to separate file

---
+++ EssentialFeed/EssentialFeediOS/ErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ErrorView.swift
+//  EssentialFeediOS
+//
+//  Created by Haider Rizvi on 29/02/2024.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
