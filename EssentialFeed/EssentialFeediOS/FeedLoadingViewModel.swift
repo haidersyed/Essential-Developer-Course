39df406361d2ec2bfe7ad80cfe08a9ed54abc11d
haidersyed <haiderrazvi@hotmail.com>
2024-02-29 17:19:13 +0500

    Move ViewModels to separate files

---
+++ EssentialFeed/EssentialFeediOS/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Haider Rizvi on 29/02/2024.
+//
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeed/EssentialFeediOS/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Haider Rizvi on 29/02/2024.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
