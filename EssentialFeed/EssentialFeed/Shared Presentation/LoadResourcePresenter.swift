47270658edb4c0ebe2d55b65f0630557ebd1df9e
haidersyed <haiderrazvi@hotmail.com>
2024-03-02 03:44:04 +0500

    Display error on mapper error

--- EssentialFeed/EssentialFeed/Shared Presentation/LoadResourcePresenter.swift
+++ EssentialFeed/EssentialFeed/Shared Presentation/LoadResourcePresenter.swift
@@ -14,7 +14,7 @@ public protocol ResourceView {
 }
 
 public final class LoadResourcePresenter<Resource, View: ResourceView> {
-    public typealias Mapper = (Resource) -> View.ResourceViewModel
+    public typealias Mapper = (Resource) throws -> View.ResourceViewModel
     
     private let resourceView: View
     private let loadingView: ResourceLoadingView
@@ -23,9 +23,9 @@ public final class LoadResourcePresenter<Resource, View: ResourceView> {
     
     public static var loadError: String {
         NSLocalizedString("GENERIC_CONNECTION_ERROR",
-            tableName: "Shared",
-            bundle: Bundle(for: Self.self),
-            comment: "Error message displayed when we can't load the resource from the server")
+                          tableName: "Shared",
+                          bundle: Bundle(for: Self.self),
+                          comment: "Error message displayed when we can't load the resource from the server")
     }
     
     public init(resourceView: View, loadingView: ResourceLoadingView, errorView: ResourceErrorView, mapper: @escaping Mapper) {
@@ -34,15 +34,19 @@ public final class LoadResourcePresenter<Resource, View: ResourceView> {
         self.errorView = errorView
         self.mapper = mapper
     }
-        
+    
     public func didStartLoading() {
         errorView.display(.noError)
         loadingView.display(ResourceLoadingViewModel(isLoading: true))
     }
     
     public func didFinishLoading(with resource: Resource) {
-        resourceView.display(mapper(resource))
-        loadingView.display(ResourceLoadingViewModel(isLoading: false))
+        do {
+            resourceView.display(try mapper(resource))
+            loadingView.display(ResourceLoadingViewModel(isLoading: false))
+        } catch {
+            didFinishLoading(with: error)
+        }
     }
     
     public func didFinishLoading(with error: Error) {
