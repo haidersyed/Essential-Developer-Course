86a212ccfb60e37bf7470f14deb0c96232c26393
haidersyed <haiderrazvi@hotmail.com>
2024-03-02 02:21:36 +0500

    Move FeedImage mapping to the FeedItemsMapper

--- EssentialFeed/EssentialFeed/Feed Api/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Api/RemoteFeedLoader.swift
@@ -13,44 +13,36 @@ public final class RemoteFeedLoader: FeedLoader {
     
     public enum Error: Swift.Error {
         case connectivity
-        case  invalidData
+        case invalidData
     }
     
     public typealias Result = FeedLoader.Result
     
     public init(url: URL, client: HTTPClient) {
-        self.client = client
         self.url = url
+        self.client = client
     }
     
     public func load(completion: @escaping (Result) -> Void) {
-        client.get(from: url){[weak self] result in
-            guard self != nil else {return}
+        client.get(from: url) { [weak self] result in
+            guard self != nil else { return }
+            
             switch result {
-            case let .success(data, response):
+            case let .success((data, response)):
                 completion(RemoteFeedLoader.map(data, from: response))
                 
-            case.failure:
+            case .failure:
                 completion(.failure(Error.connectivity))
             }
         }
     }
     
-    private static func map(_ data: Data,from response: HTTPURLResponse) -> Result {
+    private static func map(_ data: Data, from response: HTTPURLResponse) -> Result {
         do {
             let items = try FeedItemsMapper.map(data, from: response)
-            return (.success(items.toModels()))
+            return .success(items)
+        } catch {
+            return .failure(error)
         }
-        catch {
-            return (.failure(error))
-        }
-    }
-}
-
-private extension Array where Element == RemoteFeedItem {
-   func toModels() -> [FeedImage] {
-       return map {
-           FeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.image)
-       }
     }
 }
