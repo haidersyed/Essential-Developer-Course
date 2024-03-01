9dfec8a3d934d31cf9e3c3acf74130e7e5863529
haidersyed <haiderrazvi@hotmail.com>
2024-03-02 01:59:52 +0500

    Fixed error

--- EssentialApp/EssentialApp/CombineHelpers.swift
+++ EssentialApp/EssentialApp/CombineHelpers.swift
@@ -76,7 +76,7 @@ extension Publisher {
 
 extension DispatchQueue {
     static var immediateWhenOnMainQueueScheduler: ImmediateWhenOnMainQueueScheduler {
-        ImmediateWhenOnMainQueueScheduler().shared
+        ImmediateWhenOnMainQueueScheduler.shared
     }
     
     struct ImmediateWhenOnMainQueueScheduler: Scheduler {
@@ -91,14 +91,6 @@ extension DispatchQueue {
             DispatchQueue.main.minimumTolerance
         }
         
-        func schedule(options: SchedulerOptions?, _ action: @escaping () -> Void) {
-            guard isMainQueue() else {
-                return DispatchQueue.main.schedule(options: options, action)
-            }
-            
-            action()
-        }
-        
         static let shared = Self()
         
         private static let key = DispatchSpecificKey<UInt8>()
@@ -110,6 +102,14 @@ extension DispatchQueue {
         
         private func isMainQueue() -> Bool {
             DispatchQueue.getSpecific(key: Self.key) == Self.value
+        }
+        
+        func schedule(options: SchedulerOptions?, _ action: @escaping () -> Void) {
+            guard isMainQueue() else {
+                return DispatchQueue.main.schedule(options: options, action)
+            }
+                        
+            action()
         }
         
         func schedule(after date: SchedulerTimeType, tolerance: SchedulerTimeType.Stride, options: SchedulerOptions?, _ action: @escaping () -> Void) {
