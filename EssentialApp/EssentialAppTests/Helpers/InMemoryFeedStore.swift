0a4eca86d0c5acaef75d65ac3fd8393884fa43e0
haidersyed <haiderrazvi@hotmail.com>
2024-03-02 20:46:40 +0500

    Make in memory FeedImageDataStore implementation sync

--- EssentialApp/EssentialAppTests/Helpers/InMemoryFeedStore.swift
+++ EssentialApp/EssentialAppTests/Helpers/InMemoryFeedStore.swift
@@ -11,7 +11,7 @@ import EssentialFeed
 class InMemoryFeedStore {
     private(set) var feedCache: CachedFeed?
     private var feedImageDataCache: [URL: Data] = [:]
-
+    
     private init(feedCache: CachedFeed? = nil) {
         self.feedCache = feedCache
     }
@@ -22,25 +22,24 @@ extension InMemoryFeedStore: FeedStore {
         feedCache = nil
         completion(.success(()))
     }
-
+    
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
         feedCache = CachedFeed(feed: feed, timestamp: timestamp)
         completion(.success(()))
     }
-
+    
     func retrieve(completion: @escaping FeedStore.RetrievalCompletion) {
         completion(.success(feedCache))
     }
 }
 
 extension InMemoryFeedStore: FeedImageDataStore {
-    func insert(_ data: Data, for url: URL, completion: @escaping (FeedImageDataStore.InsertionResult) -> Void) {
+    func insert(_ data: Data, for url: URL) throws {
         feedImageDataCache[url] = data
-        completion(.success(()))
     }
-
-    func retrieve(dataForURL url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
-        completion(.success(feedImageDataCache[url]))
+    
+    func retrieve(dataForURL url: URL) throws -> Data? {
+        feedImageDataCache[url]
     }
 }
 
@@ -48,11 +47,11 @@ extension InMemoryFeedStore {
     static var empty: InMemoryFeedStore {
         InMemoryFeedStore()
     }
-
+    
     static var withExpiredFeedCache: InMemoryFeedStore {
         InMemoryFeedStore(feedCache: CachedFeed(feed: [], timestamp: Date.distantPast))
     }
-
+    
     static var withNonExpiredFeedCache: InMemoryFeedStore {
         InMemoryFeedStore(feedCache: CachedFeed(feed: [], timestamp: Date()))
     }
