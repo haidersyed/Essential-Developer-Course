d32ac704d16ae7e71a3067b81449764f974f7053
haidersyed <haiderrazvi@hotmail.com>
2024-03-02 19:35:30 +0500

    Remove unused code

--- EssentialFeed/EssentialFeed/Feed Presentation/FeedPresenter.swift
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedPresenter.swift
@@ -14,8 +14,5 @@ public final class FeedPresenter {
             bundle: Bundle(for: FeedPresenter.self),
             comment: "Title for the feed view")
     }
-    
-    public static func map(_ feed: [FeedImage]) -> FeedViewModel {
-        FeedViewModel(feed: feed)
-    }
+
 }
